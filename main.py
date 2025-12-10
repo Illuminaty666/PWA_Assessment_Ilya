9e6c229c4308e035188eb290a5b51a5f76ce3b0a
Illuminaty666 <132865731+Illuminaty666@users.noreply.github.com>
2025-12-10 03:16:25 +0000

    a

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
+import sqlite3
+
 
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("index.html")
+    
+    db = sqlite3.connect(".database/reviews.db")
+    db.row_factory = sqlite3.Row
+
+    table = db.execute(f"SELECT user-id,rating,game,comment,date FROM Reviews").fetchall()
+    return render_template("index.html", table=table)
 
 app.run(debug=True, port=5000)
