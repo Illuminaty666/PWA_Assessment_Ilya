c2470a3135e25dea7147fba7d74709e2f39dad26
Illuminaty666 <132865731+Illuminaty666@users.noreply.github.com>
2025-12-11 01:31:22 +0000

    attempting to make login work

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 
 app = Flask(__name__)
+
+def getdb():
+    dtb = sqlite3.connect(reviews.db)
+    dtb.row_factory = sqlite3.Row # Allows accessing rows by column name
+    return dtb
+
 @app.route("/")
 def home():
     
@@ -11,5 +17,28 @@ def home():
 
     table = db.execute(f"SELECT uid,rating,game,comment,date FROM Reviews").fetchall()
     return render_template("index.html", table=table)
+
+@app.route("/templates/add.html")
+def add():
+
+    return render_template("add.html")
+
+@app.route("/templates/reviews.html")
+def reviews():
+
+    return render_template("reviews.html")
+
+@app.route("/templates/login.html", methods=["POST"])
+def login():
+    db = sqlite3.connect(".database/reviews.db")
+    db.row_factory = sqlite3.Row 
+
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+    usertable = db.execute(f"SELECT * from Users WHERE username = username AND password=password", (username,password)).fetchone()
+
+    
+    return render_template("login.html", table=usertable)
 
 app.run(debug=True, port=5000)
