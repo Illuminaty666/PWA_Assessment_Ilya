ac328b2f7a958ba2ff6aa39bae35367dd4692919
Illuminaty666 <132865731+Illuminaty666@users.noreply.github.com>
2025-12-11 03:00:47 +0000

    a

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
 import sqlite3
 
-
 app = Flask(__name__)
+def fetchdb():
+    db = sqlite3.connect("./database/reviews.db")
+    db.row_factory = sqlite3.Row
+
+    return db
 
 @app.route("/")
 def home():
     
-    db = sqlite3.connect(".database/reviews.db")
-    db.row_factory = sqlite3.Row
-
-    table = db.execute(f"SELECT uid,rating,game,comment,date FROM Reviews").fetchall()
-    return render_template("index.html", table=table)
+    return render_template("index.html")
 
 @app.route("/templates/add.html")
 def add():
@@ -25,19 +25,12 @@ def reviews():
 
 @app.route("/templates/login.html")
 def login():
-    db = sqlite3.connect(".database/reviews.db")
-    db.row_factory = sqlite3.Row 
 
-    if request.method == "POST":
-        username = request.form.get('username')
-        password = request.form.get('password')
-    usertable = db.execute(f"SELECT * from Users").fetchall() 
-
-
-    return render_template("login.html", table=usertable)
+    return render_template("login.html")
 
 @app.route("/templates/register.html")
 def register():
+
     return render_template("register.html")
 
 
